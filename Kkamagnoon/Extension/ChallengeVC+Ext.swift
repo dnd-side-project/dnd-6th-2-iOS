85fc9e6802cb40927fc82192f7d442fec7cb96a9
nacho <hottinggot@naver.com>
2022-03-28 11:10:34 +0900

    챌린지 작성시 그림 제거

--- Kkamagnoon/Extension/ChallengeVC+Ext.swift
+++ Kkamagnoon/Extension/ChallengeVC+Ext.swift
@@ -83,7 +83,7 @@ extension ChallengeViewController {
                 owner.challengeMainView.todayKeyWordView
                     .keywordLabel.text = challenge.keyword?.content
 
-                var stamp = 1
+                let stamp = 1
 
                 // TODO: stamp 갯수 채우기
                 owner.challengeMainView.subTitleLabel[0].text = "이번달은 \(stamp)개의 스탬프를 찍었어요!"
@@ -103,6 +103,7 @@ extension ChallengeViewController {
                 if articles.count == 0 {
                     owner.setImageCaseNothing()
                 } else {
+                    owner.removeImageCaseNothing()
                     owner.challengeMainView.removeCard()
 
                     articles.forEach({ article in
@@ -145,6 +146,12 @@ extension ChallengeViewController {
             $0.top.equalTo(nothingImageView.snp.bottom).offset(9)
             $0.bottom.equalToSuperview().offset(-30.0)
         }
+    }
+
+    private func removeImageCaseNothing() {
+        challengeMainView.subTitleLabel[1].isHidden = false
+        nothingImageView.removeFromSuperview()
+        nothingLabel.removeFromSuperview()
     }
 
     private func goToWritingVC() {
