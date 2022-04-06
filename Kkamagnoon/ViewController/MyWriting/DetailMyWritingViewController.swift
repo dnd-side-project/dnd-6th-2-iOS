1411822557c47d60ef21307a6c40229bcbcab72d
nacho <hottinggot@naver.com>
2022-04-06 18:10:05 +0900

    글 상세 페이지 Label로 교체

--- Kkamagnoon/ViewController/MyWriting/DetailMyWritingViewController.swift
+++ Kkamagnoon/ViewController/MyWriting/DetailMyWritingViewController.swift
@@ -27,7 +27,7 @@ class DetailMyWritingViewController: DetailContentViewController {
             .bind { owner, article in
                 owner.detailView.titleLabel.text = article.title
                 owner.detailView.profileView.nickNameLabel.text = article.user?.nickname
-                owner.detailView.contentTextView.text = article.content
+                owner.detailView.contentLabel.text = article.content
 
                 // TODO: Created Date
 
